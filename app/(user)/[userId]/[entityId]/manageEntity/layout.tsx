b5eb11d8e1d2d31a1d058f583ac6f577ba869db0
claudeMassaad <claude1.massaad@gmail.com>
2023-03-06 15:57:13 +0200

    entity management layout segment selection

--- app/(user)/[userId]/[entityId]/manageEntity/layout.tsx
+++ app/(user)/[userId]/[entityId]/manageEntity/layout.tsx
@@ -1,7 +1,32 @@
-import { Children } from "react";
+import Link from "next/link";
+import CategoriesNavLink from "./manageEntity-Components/CategoriesNavLink";
 import ManageEntityLeftMenu from "./manageEntity-Components/ManageEntityLeftMenu";
 
 export default function Home({ children }: { children: React.ReactNode }) {
+  const userId = 1;
+
+  const managementCategories = [
+    {
+      name: "Menu",
+      route: "menuCategories",
+    },
+    {
+      name: "Exchange Rate",
+      route: "exchangeRate",
+    },
+    {
+      name: "General Information",
+      route: "entityInfo",
+    },
+    {
+      name: "Highlight Reels",
+      route: "highlightReels",
+    },
+    {
+      name: "QR Code",
+      route: "qrCode",
+    },
+  ];
   return (
     <div className="bg-gray-300 sm:h-fit sm:min-h-screen px-3 sm:px-12 pt-0 pb-7 sm:py-8">
       {/* PAGE BG COLOR AND PADDING  */}
@@ -10,7 +35,16 @@ export default function Home({ children }: { children: React.ReactNode }) {
       </div>
       <div className="flex">
         {/* LEFT MENU */}
-        <ManageEntityLeftMenu />
+        {/* <ManageEntityLeftMenu /> */}
+        <div className="hidden sm:block sm:w-[340px]">
+          <div className="rounded-lg bg-white mr-4 py-6 flex flex-col drop-shadow-lg">
+            {managementCategories.map((category) => (
+              <CategoriesNavLink categoryRoute={category.route}>
+                {category.name}
+              </CategoriesNavLink>
+            ))}
+          </div>
+        </div>
         {children}
       </div>
     </div>
---
+++ app/(user)/[userId]/[entityId]/manageEntity/manageEntity-Components/CategoriesNavLink.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
+
+export default function CategoriesNavLink({
+  categoryRoute,
+  children,
+}: {
+  categoryRoute: string;
+  children: React.ReactNode;
+}) {
+  const userId = 1;
+
+  const segment = useSelectedLayoutSegment();
+  const isActive = categoryRoute == segment;
+  return (
+    <Link
+      href={`${userId}/1/manageEntity/${categoryRoute}`}
+      className={
+        isActive
+          ? "text-blue-600 px-6 py-2 flex justify-start sm:bg-gray-100 "
+          : "text-black px-6 py-2 flex justify-start sm:focus:text-blue-600 sm:focus:bg-gray-100 sm:hover:text-blue-600 hover:bg-gray-100"
+      }
+    >
+      {children}
+    </Link>
+  );
+}

a167c70a9eeeafbe68ccdfda4466e1d8d4422b04
claudeMassaad <claude1.massaad@gmail.com>
2023-03-22 15:26:48 +0200

    entitiesBySector reading from DB

--- app/(user)/[userId]/entitiesBySector/page.tsx
+++ app/(user)/[userId]/entitiesBySector/page.tsx
@@ -1,12 +1,34 @@
 import EntitiesCardsInGridDirection from "@/app/root-Components/entityCards-Components/EntitiesCardsInGridDirection";
+import EntityCard from "@/app/root-Components/entityCards-Components/EntityCard";
+import { getAllEntitiesServer } from "@/lib/get/getAllEntities";
+import { createServerClient } from "@/utils/supabase-server";
 
-export default function EntitiesBySectorPage() {
+export default async function EntitiesBySectorPage({ userId }) {
+  //Fetch from DB
+  const supabase = createServerClient();
+  let listOfEntities = await getAllEntitiesServer(supabase);
+
+  //  const entitiesPerIndustry = listOfEntities.filter(
+  //   (entity) => entity.industry_id == props.industryId
+  // );
+
+  const verifiedEntities = listOfEntities.filter(
+    (entity) => entity.is_verified == true
+  );
+
   return (
     <div>
       <div className="flex items-center justify-between pb-2 sm:pb-4">
         <p className="font-bold text-lg sm:mx-auto">Popular Entities</p>
       </div>
-      <EntitiesCardsInGridDirection />
+
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        {verifiedEntities.map((entity) => (
+          <EntityCard entity={entity} />
+        ))}
+      </div>
+
+      {/* <EntitiesCardsInGridDirection /> */}
     </div>
   );
 }

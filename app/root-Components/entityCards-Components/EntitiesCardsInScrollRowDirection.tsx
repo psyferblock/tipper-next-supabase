a128801fe12706121ae64c494a306a7a4732655d
claudeMassaad <claude1.massaad@gmail.com>
2023-03-22 14:27:38 +0200

    filter verified entities on home page

--- app/root-Components/entityCards-Components/EntitiesCardsInScrollRowDirection.tsx
+++ app/root-Components/entityCards-Components/EntitiesCardsInScrollRowDirection.tsx
@@ -4,10 +4,14 @@ export default function EntityCardsInScrollRowDirection(props) {
   const entitiesPerIndustry = props.listOfEntities.filter(
     (entity) => entity.industry_id == props.industryId
   );
+
+  const verifiedEntities = entitiesPerIndustry.filter(
+    (entity) => entity.is_verified == true
+  );
   return (
     <>
       <div className="grid grid-rows-1 grid-flow-col gap-4 sm:gap-5 pb-2 sm:pb-5 overflow-x-auto">
-        {entitiesPerIndustry.map((entity) => (
+        {verifiedEntities.map((entity) => (
           <EntityCard entity={entity} userId={props.userId} />
         ))}
       </div>
